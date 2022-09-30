4829432c110ab85d977feb8445f4924227e16e8f
rivokashi <113569049+rivokashi@users.noreply.github.com>
2022-10-01 03:55:23 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,14 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '気温　摂氏30度で外は暑かった。　:insertx: は出かけた。:inserty: の家に着いた。　:insertz: が倒れてきた。 体重50kgのドラえもんは驚かなかったが、Kは驚いた。しずかちゃんの家でははじめてことだった。';
-//const storyText = 'It was US94 fahrenheitUK outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs US300 poundsUK, and it was a hot day.';
-const insertX = ['ドラえもん', 'アンパンマン', 'アムロ']
-//const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['しずかちゃん', 'てんどんまん', 'シャア']
-//const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['電柱', '煙突', '鉄塔']
-//const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = 'It was US94 fahrenheitUK outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs US300 poundsUK, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
@@ -32,14 +28,15 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('K', name);
+    newStory = newStory.replace('Bob', name);
   }
+  
 
-  if (document.getElementById("US").checked) {
-    const temperature =  `${Math.round(30*1.8+32)} centigrade`;
-    const weight = `${Math.round(30*2.2046)} stone`;
-    newStory = newStory.replace('摂氏30度', temperature);
-    newStory = newStory.replace('体重50kg', weight);
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
   story.textContent = newStory;
